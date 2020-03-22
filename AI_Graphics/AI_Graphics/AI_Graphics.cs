ed6e2c6196d00fd2e408f7fe132123b0c9bc4807
ore <orealterego@gmail.com>
2020-03-23 00:23:17 +0800

    Init ver

---
+++ AI_Graphics/AI_Graphics/AI_Graphics.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace AILight
+{
+    [BepInIncompatibility("dhhai4mod")]
+    [BepInDependency("RuntimeUnityEditor")]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class AILight : BaseUnityPlugin
+    {
+        public const string GUID = "ore.ai.graphics";
+        public const string PluginName = "AI Graphics";
+        public const string Version = "0.1.0";
+
+        public KeyCode ShowHotkey { get; set; } = KeyCode.F5;
+    }
+}
