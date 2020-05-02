b8155f4c9eed1aa0ca5b3cc8b64d0e8bfc83a7b4
hooh_hooah <hooh_hooah@protonmail.com>
2020-05-02 18:19:53 +0900

    Added DynamicSkyboxSettings

---
+++ AI_Graphics/AI_Graphics/Setting/DynamicSkyboxSettings.cs
@@ -0,0 +1,43 @@
+﻿using AIGraphics.Textures;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGraphics.Settings
+{
+    public class SkyboxSettings {
+        readonly static public string shaderName;
+
+        virtual public void Save() { }
+        virtual public void Load() { }
+    }
+
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class ProceduralSkyboxSettings : SkyboxSettings
+    {
+        readonly static public string shaderName = "Skybox/Procedural";
+
+        internal float sunSize;
+        internal float sunsizeConvergence;
+        internal float atmosphereThickness;
+        internal float[] skyTint;
+        internal float[] groundTint;
+        internal float exposure;
+
+        override public void Save()
+        {
+        }
+        override public void Load()
+        {
+        }
+    }
+
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class TwoPointColorSkyboxSettings : SkyboxSettings
+    {
+        readonly static public string shaderName = "SkyBox/Simple Two Colors";
+    }
+}
